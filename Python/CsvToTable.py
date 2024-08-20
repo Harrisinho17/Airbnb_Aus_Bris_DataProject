4b4e8480ebf36c2904011224d87ef62284bb2b4b
Alexander B Harris <ABelchior.Harris@gmail.com>
2024-08-20 16:45:13 -0400

    Added Pre Cleaning Data pulled from Postgres after importing the data with a Python Script to verify accuracy

--- Python/CsvToTable.py
+++ Python/CsvToTable.py
@@ -1,50 +1,16 @@
-# import pandas as pd
-# from sqlalchemy import create_engine
-
-# db_username = 'postgres'
-# db_password = 'Mudkip'
-
-# db_host = 'localhost'
-# db_port = '5432'
-# db_name = 'AirBNB.Aus.Bris'
-
-# engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
-
-# #/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data
-# df1 = pd.read_csv('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/listings.csv', low_memory=False)
-# df1.to_sql('AirBNB.Aus.Bris', engine, if_exists='replace', index=False)
-
-# df2 = pd.read_csv('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/neighbourhoods.csv', low_memory=False)
-# df2.to_sql('AirBNB.Aus.Bris', engine, if_exists='replace', index=False)
-
-# df3 = pd.read_csv('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/reviews.csv', low_memory=False)
-# df3.to_sql('AirBNB.Aus.Bris', engine, if_exists='replace', index=False)
-
-# df4 = pd.read_json('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/neighbourhoods.geojson', low_memory=False)
-# df4.to_sql('AirBNB.Aus.Bris', engine, if_exists='replace', index=False)
-
-# print("Tables created and data imported successfully!")
-
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database # type: ignore
 
 db_username = 'postgres'
 db_password = 'Mudkip'
+
 db_host = 'localhost'
 db_port = '5432'
-db_name = 'AirBNB.Aus.Bris'
+db_name = 'Brisbane.QLDAirBNB.Data'
 
 engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-# Create database if it doesn't exist
-if not database_exists(engine.url):
-    create_database(engine.url)
-    print(f'Database {db_name} created.')
-else:
-    print(f'Database {db_name} already exists.')
-
-# Load and import data
+#/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data
 df1 = pd.read_csv('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/listings.csv', low_memory=False)
 df1.to_sql('listings', engine, if_exists='replace', index=False)
 
@@ -54,7 +20,10 @@ df2.to_sql('neighbourhoods', engine, if_exists='replace', index=False)
 df3 = pd.read_csv('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/reviews.csv', low_memory=False)
 df3.to_sql('reviews', engine, if_exists='replace', index=False)
 
-df4 = pd.read_json('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/neighbourhoods.geojson', low_memory=False)
-df4.to_sql('neighbourhoods_geo', engine, if_exists='replace', index=False)
+# df4 = pd.read_json('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/neighbourhoods.geojson', low_memory=False)
+# df4.to_sql('Brisbane.QLDAirBNB.Data', engine, if_exists='replace', index=False)
+
+df4 = pd.read_csv('/Users/aharris/Documents/GitHub/Airbnb_Aus_Bris_DataProject/Airbnb Data/calendar.csv', low_memory=False)
+df4.to_sql('calendar', engine, if_exists='replace', index=False)
 
 print("Tables created and data imported successfully!")
